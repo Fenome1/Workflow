b96e50b9fdbfd72e858aa53bede411a5cfdb11fa
Fenome1 <warfaceserii@mail.ru>
2024-04-14 13:43:10 +0300

    fix

--- workflow.back/Workflow.Api/Module/ApiModule.cs
+++ workflow.back/Workflow.Api/Module/ApiModule.cs
@@ -1,5 +1,9 @@
-﻿using Autofac;
+﻿using System.Text;
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Workflow.Application.Services.TokenService;
 using Workflow.Persistense.Context;
 
 namespace Workflow.Api.Module;
@@ -13,6 +17,26 @@ public class ApiModule(IConfiguration configuration) : Autofac.Module
         services.AddDbContext<WorkflowDbContext>();
 
         services.AddSignalR();
+        
+        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+
+        var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
+
+        services.ConfigureApplicationCookie(options => { options.Cookie.SameSite = SameSiteMode.None; })
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions!.SecretKey))
+                };
+            });
+
+        services.AddAuthorization();
 
         builder.Populate(services);
     }
