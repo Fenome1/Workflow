fec14edfac53f755a857dc110e9d97cadb373ea7
Fenome1 <warfaceserii@mail.ru>
2024-05-16 21:01:40 +0300

    config server ip rout add

--- workflow.web.app/config.ts
+++ workflow.web.app/config.ts
@@ -1,2 +1,5 @@
+/*export const BASE_API_URL = "http://31.128.38.142:5050/Api";
+export const BASE_HUB_URL = "http://31.128.38.142:5050";*/
+
 export const BASE_API_URL = "http://localhost:5038/Api";
 export const BASE_HUB_URL = "http://localhost:5038";
