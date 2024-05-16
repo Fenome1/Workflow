52ad1565776bff122414e9d50287aab5cc05bdb1
Fenome1 <warfaceserii@mail.ru>
2024-05-16 22:54:01 +0300

    master fix settings

--- workflow.web.app/config.ts
+++ workflow.web.app/config.ts
@@ -1,6 +1,7 @@
+/*
 export const BASE_API_URL = "http://31.128.38.142:5050/Api";
 export const BASE_HUB_URL = "http://31.128.38.142:5050";
+*/
 
-/*
 export const BASE_API_URL = "http://localhost:5038/Api";
-export const BASE_HUB_URL = "http://localhost:5038";*/
+export const BASE_HUB_URL = "http://localhost:5038";
