973fd88f5d358b94bc8056a365b24c043b84ea4d
Fenome1 <warfaceserii@mail.ru>
2024-05-17 11:33:19 +0300

    deploy

--- workflow.web.app/config.ts
+++ workflow.web.app/config.ts
@@ -1,7 +1,5 @@
-/*
 export const BASE_API_URL = "http://31.128.38.174:5050/Api";
 export const BASE_HUB_URL = "http://31.128.38.174:5050";
-*/
 
-export const BASE_API_URL = "http://localhost:5038/Api";
-export const BASE_HUB_URL = "http://localhost:5038";
+/*export const BASE_API_URL = "http://localhost:5038/Api";
+export const BASE_HUB_URL = "http://localhost:5038";*/
